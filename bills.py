8e0a3d84bdcca15eea2bdcf84ba22967ec1d10d6
Michelle Orden <morden@uchicago.edu>
2022-10-25 11:29:24 -0500

    added additional print statements. added num_bills to args

--- bills.py
+++ bills.py
@@ -3,10 +3,6 @@ import requests
 import json
 import math
 from os.path import exists
-# import re
-# import time
-# import sys
-# import boto3
 
 API_KEY = 'nt5nhSpwSqMbrGJ7hcsBkXFI1mfk80X0fexbnt45'
 
@@ -22,69 +18,77 @@ API_KEY = 'nt5nhSpwSqMbrGJ7hcsBkXFI1mfk80X0fexbnt45'
 #                    'rfh', 'rfs', 'rh', 'rih', 'ris', 'rs', 'rth', 'rts', 'sas',
 #                    'sc']
 
-def get_bill_ids(congress='116',
+def get_bill_ids(num_bills=25,
+                 congress='116',
                  docClass='s',
-                 billVersion='is',
-                 lastModifiedStartDate='1990-05-13T02:22:08Z',
-                 offset=0,
-                 pageSize=1000):
+                 billVersion='is'):
     '''
     For the given  inputs, this function makes a govinfo
     API call to request a list of 'collections' (bill ids).
     The requested bill ids are saved as json to disk.
 
     Inputs:
+        num_bills (int) - The number of bill texts to retrieve (default 25).
         congress (str) - congress number (116 default)
         docClass (str) - bill/collection categories (s, hr, hres, sconres)
         billVersion (str) - the bill version (there are 53 possible types)
-        lastModifiedStartDate (str) - This is the start date and time in
-            ISO8601 format (yyyy-MM-dd'T'HH:mm:ss'Z')
-        offset (int) - This is the starting record you wish to retrieve
-        pageSize (int) - The number of records you would like to return within
-            a given request
     Returns:
         if the request succeeds, data (dictionary) is saved to disk
-            and get_package() is called
+            and get_bills() is called
         if the requests does not succeed, returns status code
     '''
+    if num_bills > 999:
+        print("Max number of API requests reached. Try again with fewer bills.")
+        return 400
+
     bill_id_file = (f"data/ids/{congress}/{congress}_{docClass}_{billVersion}"
                     ".json")
     if exists(bill_id_file):
         # Skip getting bill ids
         # Just get bill texts
-        return get_package(bill_id_file)
-    
+        print(f"Id file already exitst for congress '{congress}', docClass "
+              f"'{docClass}', billVersion '{billVersion}'.")
+        return get_bills(bill_id_file, int(num_bills))
+
+    lastModifiedStartDate='1990-05-13T02:22:08Z'
+    offset=0
+    pageSize=1000
     url = (f'https://api.govinfo.gov/collections/BILLS/{lastModifiedStartDate}'
            f'?offset={offset}&pageSize={pageSize}&congress={congress}&docClass'
            f'={docClass}&billVersion={billVersion}&api_key={API_KEY}')
     PARAMS = {'headers': 'accept: application/json'}
 
+    print(f"Requesting bill ids for congress '{congress}', docClass '{docClass}', "
+          f"billVersion '{billVersion}'.")
     r = requests.get(url=url, params=PARAMS)
     if r.status_code == 200:
         data = r.json()
+        # Save bill ids to disk
         with open((f"data/ids/{congress}/{congress}_{docClass}_{billVersion}"
                    ".json"), "w") as outfile:
             json.dump(data, outfile)
         print(f"{data['count']} bill ids for congress '{congress}', docClass "
               f"'{docClass}', billVersion '{billVersion}' saved to disk.")
         # Get bill texts
-        return get_package(outfile.name)
+        return get_bills(outfile.name, int(num_bills))
     print(f"Status code: {r.status_code}")
     return r.status_code
 
 
-def get_package(bill_id_file):
+def get_bills(bill_id_file, num_bills):
     '''
     Given a file containing bill ids, this function makes a govinfo API call to
     request the bill text.
 
     Inputs:
         bill_id_file (str) - Path to a file containing json with all bill ids
+        num_bills (int) - The number of bill texts to retrieve (default 25).
     Returns:
         This function dumps pdf files containing bill texts into their
         respective folders on disk.
     '''
     congress = bill_id_file.split("/")[2]
+    # Read in file with bill ids
     with open(bill_id_file, "rb") as read_content:
         id_content = json.load(read_content)
         all_bill_ids = id_content['packages']
@@ -93,16 +97,16 @@ def get_package(bill_id_file):
     # The API allows a maximum of 1000 requests per hour for a given API key.
     max_bills = 999
     num_machines = math.ceil(bill_count / max_bills)
-    print(f"{num_machines} machines required to get all {bill_count} bills in "
-          "1 hour.")
-    print("As a trial, we will get the first 10 bills.")
+    print(f"At least {num_machines} machines required to reqeust all {bill_count} bills.")
+    print(f"Requesting {num_bills} bill texts.")
 
     bill_ids_saved = []
     for bill in all_bill_ids:
-        if len(bill_ids_saved) == 10:
+        if len(bill_ids_saved) == num_bills:
             break
         bill_id = bill["packageId"]
         bill_pdf_path = f"data/bills/{congress}/{bill_id}.pdf"
+        # Don't request bill that is already saved
         if exists(bill_pdf_path):
             continue
         bill_ids_saved.append(bill_id)
@@ -110,10 +114,11 @@ def get_package(bill_id_file):
                f"{API_KEY}")
         PARAMS = {'headers': 'accept: */*'}
         r = requests.get(url = url, params = PARAMS)
+        # Save bill pdf
         with open(bill_pdf_path, "wb") as outfile:
             outfile.write(r.content)
     print(f"{len(bill_ids_saved)} bill texts (.pdfs) for congress '{congress}'"
-          f"saved to disk: {bill_ids_saved}")
+          f" saved to disk: {bill_ids_saved}")
 
 
 if __name__ == '__main__':
